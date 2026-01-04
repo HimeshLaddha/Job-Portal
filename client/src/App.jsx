7d492b7a15917866dc516ea0be14ac3c7d844496
HimeshLaddha <himeshladdha@gmail.com>
2026-01-04 20:19:04 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Applications from './pages/Applications'
+import ApplyJob from './pages/ApplyJob'
+import RecruiterLogin from './components/RecruiterLogin'
+import { useContext } from 'react'
+import { AppContext } from './context/AppContext'
+import Dashboard from './pages/Dashboard'
+import AddJobs from './pages/AddJobs'
+import ManageJobs from './pages/ManageJobs'
+import ViewApplications from './pages/viewApplications'
+
+function App() {
+
+  const { showRecruiterLogin } = useContext(AppContext)
+
+  return (
+    <>
+      <div>
+        {
+          showRecruiterLogin && <RecruiterLogin />
+        }
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/applications' element={<Applications />} />
+          <Route path='/apply-job/:id' element={<ApplyJob />} />
+          <Route path='/dashboard' element={<Dashboard />}>
+            <Route path='add-job' element={<AddJobs />} />
+            <Route path='manage-jobs' element={<ManageJobs />} />
+            <Route path='view-applications' element={<ViewApplications />} />
+          </Route>
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
