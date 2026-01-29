2290fd6daa0e230e7eab27e028655131da0ad809
Himesh Laddha <himeshladdha@gmail.com>
2026-01-29 23:24:40 +0530

    Fix Sentry crash using lazy initialization on Vercel

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ import express from "express"
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./config/db.js";
-import './config/instrument.js'
-import * as Sentry from "@sentry/node"
+// import './config/instrument.js'
+// import * as Sentry from "@sentry/node"
 import { clerkWebhooks } from "./controllers/webhooks.js";
 
 // initialise express
@@ -28,8 +28,20 @@ app.post('/webhooks', clerkWebhooks)
 // port
 const PORT = process.env.PORT || 5000;
 
-Sentry.setupExpressErrorHandler(app);
+// Sentry.setupExpressErrorHandler(app);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running on PORT : ${PORT}`)
+
+    if (process.env.NODE_ENV === "production") {
+        try {
+            const Sentry = await import("@sentry/node");
+            await import("./config/instrument.js");
+
+            Sentry.setupExpressErrorHandler(app);
+            console.log("Sentry initialized safely");
+        } catch (err) {
+            console.error("Sentry failed to initialize:", err.message);
+        }
+    }
 })
